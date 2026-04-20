0058ae5a09d045633e7046314fb76d27672b44ec
NourSfar <sfarnour72@gmail.com>
2026-04-20 11:16:05 +0100

    feat: swithing to mistral model/provider because of openrouter lantency and free calls rate limit

--- src/common/utils/llm-provider.factory.ts
+++ src/common/utils/llm-provider.factory.ts
@@ -1,6 +1,6 @@
 import { ConfigService } from '@nestjs/config';
-//import { createMistral } from '@ai-sdk/mistral';
-import { createOpenRouter } from '@openrouter/ai-sdk-provider';
+import { createMistral } from '@ai-sdk/mistral';
+//import { createOpenRouter } from '@openrouter/ai-sdk-provider';
 import { LanguageModel } from 'ai';
 
 /**
@@ -12,14 +12,14 @@ import { LanguageModel } from 'ai';
  */
 export function createLlmModel(configService: ConfigService): LanguageModel {
   // ─── OpenRouter (Default) ────────────────────────────────────────────────────
-  const openrouter = createOpenRouter({
-    apiKey: configService.get<string>('OPENROUTER_API_KEY'),
-  });
-  return openrouter.chat(configService.get<string>('OPENROUTER_MODEL')!);
+  // const openrouter = createOpenRouter({
+  //   apiKey: configService.get<string>('OPENROUTER_API_KEY'),
+  // });
+  // return openrouter.chat(configService.get<string>('OPENROUTER_MODEL')!);
 
   // ─── Mistral (Alternative) ───────────────────────────────────────────────────
-  // const mistral = createMistral({
-  //   apiKey: configService.get<string>('MISTRAL_API_KEY'),
-  // });
-  // return mistral(configService.get<string>('MISTRAL_MODEL')!);
+  const mistral = createMistral({
+    apiKey: configService.get<string>('MISTRAL_API_KEY'),
+  });
+  return mistral(configService.get<string>('MISTRAL_MODEL')!);
 }
